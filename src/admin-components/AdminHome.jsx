b2ee7e3c6d9d2b2b574e6aa4fade02aac86d5e5e
mmwandera <mmwandera74@gmail.com>
2024-05-02 11:42:11 +0300

    styled the admin header component

--- src/admin-components/AdminHome.jsx
+++ src/admin-components/AdminHome.jsx
@@ -1,5 +1,23 @@
+import Card from './reusable-components/Card';
+import Footer from './reusable-components/Footer';
+import Header from './reusable-components/Header';
+import SearchBar from './reusable-components/SearchBar';
+
 export default function AdminHome() {
   return (
-    <div>Admin Home</div>
-  )
+    <div className="admin-home">
+      <Header />
+      <main className="admin-main">
+        <h1 className="welcome-message">Welcome, User</h1>
+        <div className="search-bar-container">
+          <SearchBar />
+        </div>
+        <h2 className="courses-heading">My Courses</h2>
+        <div className="card-container">
+          <Card />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }

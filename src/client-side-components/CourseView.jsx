9257fa634da43dd5b4b933d8e2962a7d6132dc75
mmwandera <mmwandera74@gmail.com>
2024-05-11 11:35:12 +0300

    added course view layout

--- src/client-side-components/CourseView.jsx
+++ src/client-side-components/CourseView.jsx
@@ -1,5 +1,30 @@
+import './courseView.css';
+// import dummyData from './dummy-data/course-view-data.json';
+// import { useEffect, useState } from 'react';
+// import CourseModule from './reusable-components/CourseModule'
+// import ModuleDetails from './reusable-components/ModuleDetails'
+
 export default function CourseView() {
+  const [courseView, setcourseView] = useState([]);
+
+  
+
   return (
-    <div>CourseView</div>
-  )
+    <div className="course-view">
+      <div className="course-title">
+        <h1>Course Title</h1>
+      </div>
+      <div className="course-content">
+        <div className="course-modules">
+          <h2>Modules</h2>
+          {}
+        </div>
+        <div className="course-media">
+          <h2>Notes</h2>
+          {/* Render media and notes here */}
+        </div>
+      </div>
+    </div>
+  );
 }
+
